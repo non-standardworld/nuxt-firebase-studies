d6f0ae29c5780972f51774f2d74a1c89e1c27f19
Kenji Takasaki <kenji@non-standardworld.co.jp>
2018-08-13 15:22:25 +0900

    Firebaseのデータの書き込み、読み込み

--- components/Mypage.vue
+++ components/Mypage.vue
@@ -1,7 +1,17 @@
 <template>
 <div id="mypage">
   <span>こんにちは, {{ user.displayName }}さん</span>
-  <button @click="logout">ログアウト</button>
+  <p><button @click="logout">ログアウト</button></p>
+  <h2>入力されたノート</h2>
+  <p>{{ note_content }}</p>
+  <p><textarea v-model="note_content"></textarea></p>
+    <p><button class="saveNoteBtn" @click="saveContent(note_content)">ノートの保存</button></p>
+  <h2>保存されたノート</h2>
+  <div class="noteList" v-for="note in notes" :key="note.content">
+    <p class="noteContent">{{ note }}</p>
+  </div>
+
+
 </div>
 </template>
 
@@ -10,9 +20,30 @@ import firebase from '@/plugins/firebase'
 export default {
   name: 'mypage',
   props: ['user'],
+  data (context) {
+    // コンポーネントをロードする前に毎回呼び出されます
+    return { note_content: 'hello', notes:[]}
+  },
+  created: function() {
+    firebase
+      .database()
+      .ref('notes/' + this.user.uid)
+      .once('value')
+      .then(result => {
+        if (result.val()) {
+          this.notes = result.val();
+        }
+      })
+  },
   methods: {
     logout: function() {
       firebase.auth().signOut();
+    },
+    saveContent: function(value) {
+      firebase
+        .database()
+        .ref('notes/' + this.user.uid)
+        .set({content:value});
     }
   }
 }
